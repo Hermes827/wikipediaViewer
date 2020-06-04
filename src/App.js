61d4ea8dba4a74662a97979fd7b754590d285b91
Hermes827 <ccable827@gmail.com>
2020-06-04 09:05:37 -0700

    made basic framework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import glass from './pictures/glass.svg.png'
 
-function App() {
+class App extends React.Component {
+
+  constructor(){
+    super()
+  }
+
+  hello(){
+    console.log("hello")
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h3>Click here for a random article</h3>
+    <img className="glass" src={glass} onClick={this.hello}></img>
+    <h3>Click icon to search</h3>
     </div>
   );
+}
 }
 
 export default App;
