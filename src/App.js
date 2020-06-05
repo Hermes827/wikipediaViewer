fbbd1a4885844385031c7263687278e24de6c69a
github username <github email address>
2020-06-05 10:01:26 -0700

    created searchbar rendering functionality

--- src/App.js
+++ src/App.js
@@ -2,16 +2,59 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import glass from './pictures/glass.svg.png'
+import Searchbar from './components/searchbar.js'
+const wikiAPI = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=%27New_England_Patriots%27"
 
 class App extends React.Component {
 
   constructor(){
     super()
+    this.state = {
+      hasClickedButton: false,
+      array: []
+    }
+
+    this.hello = this.hello.bind(this)
+    this.renderSearchBar = this.renderSearchBar.bind(this)
   }
 
+
+//   hello = () => {
+//     this.setState({
+//       hasClickedButton: true
+//     })
+//     console.log(this.state)
+//   }
+// //the above arrow function is able to access the "this" context of this class, if it was defined only as a
+// //regular function then it wouldn't be able to access the 'this' context of this class because the function
+// //has its own 'this' context
+
+
   hello(){
-    console.log("hello")
+    this.setState({
+      hasClickedButton: true
+    })
+
   }
+
+  renderSearchBar(){
+    if(this.state.hasClickedButton === true){
+      return (
+        <Searchbar/>
+      )
+    }
+  }
+
+
+  // render(){
+  //  return(
+  //   <div>
+  //       {(!this.state.isEdit) ? <Display /> : <EditForm />}
+  //    </div>
+  //      )
+  //    }
+
+
 
   randomArticle(){
     window.open("https://en.wikipedia.org/wiki/Special:Random")
@@ -21,7 +64,7 @@ class App extends React.Component {
   return (
     <div className="App">
     <h3 onClick={this.randomArticle}>Click here for a random article</h3>
-    <img className="glass" src={glass} onClick={this.hello}></img>
+    {(this.state.hasClickedButton ===true) ? <Searchbar/> :  <img className="glass" src={glass} onClick={this.hello}></img>}
     <h3>Click icon to search</h3>
     </div>
   );
---
+++ src/test.js
@@ -0,0 +1,120 @@
+renderNews(){
+    if(this.props.history.location.pathname === "/news" && this.state.current_user.username){
+      return (
+        <NewsContainer/>
+      )
+    }
+  }
+
+  import React, { Component } from 'react'
+import Article from '../components/article'
+import {withRouter} from 'react-router';
+import ArticleView from '../components/articleView'
+
+const NEWS_API= "https://newsapi.org/v2/everything?q=cryptocurrency&from=2019-12-10&sortBy=publishedAt&apiKey=e17454af05b842518705a1a4960a4f94"
+
+
+class NewsContainer extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      news: [],
+      currentNewsArticle: {},
+      lookingAtSingleNewsArticle: false,
+      hasClickedButton: false
+    }
+  }
+
+  componentDidMount(){
+    this.startLoadingSpinner()
+    fetch(NEWS_API)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          news: data.articles
+        })
+        this.stopLoadingSpinner()
+      })
+  }
+
+  setCurrentNewsArticle = (article) => {
+    this.setState({
+      currentNewsArticle: article,
+      lookingAtSingleNewsArticle: true
+    })
+  }
+
+  renderDetailedNewsView = () => {
+  const {currentNewsArticle, lookingAtSingleNewsArticle} = this.state
+  if(lookingAtSingleNewsArticle === true) {
+      return (
+            <ArticleView
+            currentNewsArticle={currentNewsArticle}
+            returnToNewsContainer={this.returnToNewsContainer}
+            />
+          )
+    }
+  }
+
+  returnToNewsContainer = () => {
+    this.setState({
+      currentNewsArticle: null,
+      lookingAtSingleNewsArticle: false
+    })
+  }
+
+  returnToHomepageFromNewsContainer = () => {
+    this.setState({
+      currentNewsArticle: null,
+      lookingAtSingleNewsArticle: false
+    })
+    this.props.history.push('/center_console')
+  }
+
+  // loading spinner
+
+  startLoadingSpinner(){
+    this.setState({
+      hasClickedButton: true
+    })
+  }
+
+  stopLoadingSpinner(){
+    this.setState({
+      hasClickedButton: false
+    })
+  }
+
+  renderLoadingSpinner(){
+    if(this.state.hasClickedButton === true){
+      return (
+        <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+      )
+    }
+  }
+
+  render(){
+    return (
+      <div className="newsContainer">
+      <h1 className="ccHeader">News Stories</h1>
+      <button className="CCbutton" onClick={this.returnToHomepageFromNewsContainer}>Return to homepage</button>
+      {this.renderDetailedNewsView()}
+      <div>
+        {this.renderLoadingSpinner()}
+        {
+          this.state.news.map((article)=> {
+            return <Article
+                    className="article"
+                    article={article}
+                    setCurrentNewsArticle={this.setCurrentNewsArticle}
+                    />
+          })
+        }
+      </div>
+      </div>
+    )
+  }
+}
+
+export default withRouter(NewsContainer)
