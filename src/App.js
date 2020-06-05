c5b63b031a1107b7cf62a07a20418bfcbf9461ad
github username <github email address>
2020-06-05 10:24:33 -0700

    toggle searchbar function fully functional

--- src/App.js
+++ src/App.js
@@ -14,47 +14,12 @@ class App extends React.Component {
       array: []
     }
 
-    this.hello = this.hello.bind(this)
-    this.renderSearchBar = this.renderSearchBar.bind(this)
+    this.toggleState = this.toggleState.bind(this)
   }
 
-
-//   hello = () => {
-//     this.setState({
-//       hasClickedButton: true
-//     })
-//     console.log(this.state)
-//   }
-// //the above arrow function is able to access the "this" context of this class, if it was defined only as a
-// //regular function then it wouldn't be able to access the 'this' context of this class because the function
-// //has its own 'this' context
-
-
-  hello(){
-    this.setState({
-      hasClickedButton: true
-    })
-
+  toggleState(){
+    (this.state.hasClickedButton === true) ? this.setState({hasClickedButton: false}) : this.setState({hasClickedButton: true})
   }
-
-  renderSearchBar(){
-    if(this.state.hasClickedButton === true){
-      return (
-        <Searchbar/>
-      )
-    }
-  }
-
-
-  // render(){
-  //  return(
-  //   <div>
-  //       {(!this.state.isEdit) ? <Display /> : <EditForm />}
-  //    </div>
-  //      )
-  //    }
-
-
 
   randomArticle(){
     window.open("https://en.wikipedia.org/wiki/Special:Random")
@@ -64,8 +29,9 @@ class App extends React.Component {
   return (
     <div className="App">
     <h3 onClick={this.randomArticle}>Click here for a random article</h3>
-    {(this.state.hasClickedButton ===true) ? <Searchbar/> :  <img className="glass" src={glass} onClick={this.hello}></img>}
+    {(this.state.hasClickedButton ===true) ? <Searchbar toggle={this.toggleState}/> : <img className="glass" src={glass} onClick={this.toggleState}></img>}
     <h3>Click icon to search</h3>
+    {console.log(this.state)}
     </div>
   );
 }
