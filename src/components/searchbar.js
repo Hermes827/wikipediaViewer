85b531d92802e90f58a1071a1d5a3509b22dc3d5
Hermes827 <ccable827@gmail.com>
2020-06-05 10:24:33 -0700

    toggle searchbar function fully functional

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -7,77 +7,15 @@ class Searchbar extends React.Component {
 
   }
 
-  componentDidMount(){
-    // this.startLoadingSpinner()
-    // fetch(NEWS_API)
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     this.setState({
-    //       news: data.articles
-    //     })
-    //     this.stopLoadingSpinner()
-    //   })
-  }
-
-  // setCurrentNewsArticle = (article) => {
-  //   this.setState({
-  //     currentNewsArticle: article,
-  //     lookingAtSingleNewsArticle: true
-  //   })
-  // }
-  //
-  // renderDetailedNewsView = () => {
-  // const {currentNewsArticle, lookingAtSingleNewsArticle} = this.state
-  // if(lookingAtSingleNewsArticle === true) {
-  //     return (
-  //           <ArticleView
-  //           currentNewsArticle={currentNewsArticle}
-  //           returnToNewsContainer={this.returnToNewsContainer}
-  //           />
-  //         )
-  //   }
-  // }
-
-  returnToNewsContainer = () => {
-    this.setState({
-      currentNewsArticle: null,
-      lookingAtSingleNewsArticle: false
-    })
-  }
-
-  returnToHomepageFromNewsContainer = () => {
-    this.setState({
-      currentNewsArticle: null,
-      lookingAtSingleNewsArticle: false
-    })
-    this.props.history.push('/center_console')
-  }
-
-  // loading spinner
-
-  // startLoadingSpinner(){
-  //   this.setState({
-  //     hasClickedButton: true
-  //   })
-  // }
-  //
-  // stopLoadingSpinner(){
-  //   this.setState({
-  //     hasClickedButton: false
-  //   })
-  // }
-
-  // renderLoadingSpinner(){
-  //   if(this.state.hasClickedButton === true){
-  //     return (
-  //       <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-  //     )
-  //   }
-  // }
-
   render(){
     return (
-      <div>whats up</div>
+      <div>
+      <form>
+      <input/>
+      <button onClick={this.toggleState}>cancel</button>
+      {console.log(this.props)}
+      </form>
+      </div>
     )
   }
 }
