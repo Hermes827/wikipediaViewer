2ba78e24c46add4a1f4ae0c891812befc6eb47fc
Hermes827 <ccable827@gmail.com>
2020-06-05 11:00:15 -0700

    currently implementing API connectivity

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -4,16 +4,43 @@ class Searchbar extends React.Component {
 
   constructor(){
     super()
-
+    this.state = {
+      text: ""
+    }
+    this.captureText = this.captureText.bind(this)
+    this.executeSearch = this.executeSearch.bind(this)
   }
+
+  captureText(event){
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  executeSearch(){
+    // console.log(this.state.text)
+    // console.log("hello")
+    const weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=47.4797&lon=-122.2079&units=imperial&appid=2ebf31cbd8d1d525b4f75bedc92f60f0`;
+    fetch(weatherApi)
+   .then(response => response.json())
+   .then(data => {
+      console.log(data)
+     })
+  }
+
+  //API not calling for some reason
+
+
+
+  //I just need to add the ability to interpolate the wiki api and this should work
+  //perhaps use window.open function with the finished api?
 
   render(){
     return (
       <div>
-      <form>
-      <input/>
+      <form onSubmit={this.executeSearch}>
+      <input type='text' name='text' value={this.state.text} onChange={this.captureText}/>
       <button onClick={this.toggleState}>cancel</button>
-      {console.log(this.props)}
       </form>
       </div>
     )
